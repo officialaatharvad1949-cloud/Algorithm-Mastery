5203f9be4ca0175bc1f302eb0688777d0c55b561
Atharva Dhomane <officialaatharvad1949@gmail.com>
2026-07-03 00:23:21 +0530

    Add greedy algorithm for valid parentheses check
    Implemented a greedy algorithm to check valid parentheses with support for range-based greedy approach(using two variable min and max).

--- Greedy-Algo/678.Valid_Parenthesis.cpp
+++ Greedy-Algo/678.Valid_Parenthesis.cpp
@@ -1,1 +1,27 @@
-
+class Solution {
+    // This solution is based on Greedy Algorithm. Instead of just the cnt variable filling up the asterisk
+    // here , a range of value will be maintained .
+    // if range contains the desired value, then it's true, otherwise false;
+    // Time complexity is big O(N) where  N is the size of string;
+public:
+    bool checkValidString(string s) {
+        int min = 0 , max = 0;
+        for(int i = 0 ; i < s.size() ; i++){
+            if(s[i] == '('){
+                min +=1;
+                max += 1;
+            }else if(s[i] == ')'){
+                min -= 1;
+                max -= 1;
+            }
+            else if(s[i] == '*'){
+                min -= 1;
+                max += 1;
+            }
+            if(min < 0) min = 0;
+            if(max < 0) return false;
+        }
+        return (min == 0);
+        
+    }
+};

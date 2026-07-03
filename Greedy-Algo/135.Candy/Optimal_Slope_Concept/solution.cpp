90420cd988fb1c39ad91cf20d280360583a56261
Atharva Dhomane <officialaatharvad1949@gmail.com>
2026-07-03 18:27:52 +0530

    Add candy distribution solution based on ratings
    Implement candy distribution logic based on ratings.

--- Greedy-Algo/135.Candy/Optimal_Slope_Concept/solution.cpp
+++ Greedy-Algo/135.Candy/Optimal_Slope_Concept/solution.cpp
@@ -1,1 +1,33 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        //your code goes here
+        int n = ratings.size();
+        int sum = 1 , i = 1 ;
+        while(i < n){
+            if(ratings[i] == ratings[i-1]){
+                sum += 1;
+                i += 1;
+                continue;
+            }
+           int peak = 1;
+            while(i < n && ratings[i] > ratings[i-1]){
+                peak += 1;
+                sum += peak;
+               
+                i += 1;
+            }
+            int down = 1;
+            while(i < n && ratings[i] < ratings[i-1]){
+                sum += down ;
+                down += 1;
+                i += 1;
+            }
+            if(down > peak){
+                sum += down-peak;
+            }
 
+        }
+        return sum;
+    }
+};
